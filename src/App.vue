e989a16da663e2c90f82523d4c4f1ce3dfd29f59
Ben Pluim <benpluim@Bens-MacBook-Pro.local>
2022-12-04 21:04:18 -0700

    Creating Repo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid main">
+    <div class="row imgBg">
+      <div class="marBg col-sm-4 col-md-3" />
+      <div class="titleDetails">
+        <h1>Explore</h1>
+        <div class="moreDetails" :class={} data-bs-toggle="modal" data-bs-target="#exampleModal" >
+          <img class="moreBtn" src="./assets/Plus.svg"/>
+          <p>More Details</p>
+        </div>
+      </div>
+      <div class="socials col-8">
+        <img class="facebook" src="./assets/Facebook.svg" />
+        <img class="instagram" src="./assets/Instagram.svg" />
+      </div>
+    </div>
+    <ExploreModal title="Explore" :body="modalBody"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ExploreModal from './components/ExploreModal.vue'
+
+const modalBody = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim adminima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ExploreModal
+  },
+  data() {
+    return {
+      modalBody,
+    }
+  },
+  methods: {
+    removeMainTitle() {
+      
+    }
   }
 }
 </script>
@@ -20,7 +47,95 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  /*padding-top: 60px;*/
 }
+
+.main {
+  /*background: #611818;*/
+  height: 100vh;
+  width: 100vw;
+}
+
+.marBg {
+  background: #611818;
+  height: 100vh;
+}
+
+.imgBg {
+  background-image: url('./assets/Forest.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+}
+
+.titleDetails {
+  width: auto;
+  position: absolute;
+  text-align: left;
+  top: 50%;
+  left: 15%;
+}
+
+.titleDetails h1 {
+  font-size: 4rem;
+  font-family: 'Libre Bodoni', serif;
+}
+
+.moreDetails {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+}
+
+.moreDetails:hover {
+  cursor: pointer;
+}
+
+.moreDetails p {
+  margin-left: 15px;
+  margin-bottom: 0;
+}
+
+.moreBtn {
+  height: 35px;
+  width: 35px;
+}
+
+.moreBtn:hover {
+  cursor: pointer;
+}
+
+.socials {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: flex-end;
+}
+
+.socials img {
+  height: 50px;
+  width: 50px;
+  margin: 15px 0;
+}
+
+@media (min-width: 692px) {
+  .titleDetails h1 {
+    font-size: 7rem;
+  }
+
+  .moreDetails {
+    margin-left: 35px;
+  }
+}
+
+@media (min-width: 962px) {
+  .titleDetails h1 {
+    font-size: 10rem;
+   }
+}
+
 </style>
---
+++ src/components/ExploreModal.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <div class="close-btn">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">{{ title }}</h1>
+                    <hr />
+                </div>
+                <div class="modal-body">
+                    <p>{{ body }}</p>
+                </div>
+                <div class="modal-footer">
+                    <button
+                        type="button"
+                        class="btn"
+                        data-bs-dismiss="modal"
+                        onclick="window.location.href='https://www.google.com';">
+                        Read more
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ExploreModal',
+    props: {
+        title: String,
+        body: String,
+    }
+}
+</script>
+
+<style scoped>
+.modal {
+    color: black;
+    width: 75%;
+    top: 50px;
+    left: 50px;
+    height: auto;
+}
+
+.modal-header {
+    flex-flow: column nowrap;
+    border-bottom: none;
+}
+
+.modal-header h1 {
+    font-size: 2rem !important;
+    font-family: 'Libre Bodoni', serif;
+    color: #611818;
+}
+
+.close-btn {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+}
+
+.btn-close[aria-label] {
+    color: #611818;
+}
+
+.modal-title, .modal-body, .modal-footer {
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+}
+
+.modal-body {
+    text-align: left;
+    border-bottom: none !important;
+}
+
+.modal-body p {
+    font-size: .75rem;
+}
+
+.modal-footer {
+    border-top: none !important;
+}
+
+.btn {
+    background: linear-gradient(to right, #611818, #9f5252);
+    color: white;
+}
+
+@media (min-width: 692px) {
+    .modal {
+        width: 650px;
+        top: 150px;
+    }
+
+    .modal-body p {
+    font-size: 1.5rem;
+}
+}
+</style>
