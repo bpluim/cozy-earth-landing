125891121cae397414836077295f2e47e57454a4
Ben Pluim <benpluim@Bens-MacBook-Pro.local>
2022-12-04 21:51:35 -0700

    remove unnecessary text

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
     </div>
     <ExploreModal title="Explore" :body="modalBody"/>
   </div>
-</template>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
+</template>
 
 <script>
 import ExploreModal from './components/ExploreModal.vue'
